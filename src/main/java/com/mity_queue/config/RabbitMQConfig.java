2abc5fda8dd1be639c52690e3538d53a8c1c1bf1
Gyanendra Kumar <gyanendrak874@gmail.com>
2024-03-22 15:52:02 +0530

    Added Configuration for rabbitmq

---
+++ src/main/java/com/mity_queue/config/RabbitMQConfig.java
@@ -0,0 +1,39 @@
+package com.mity_queue.config;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+  @Value("${rabbitmq.queue.name}")
+  private String queue;
+
+  @Value("${rabbitmq.exchange.name}")
+  private String exchange;
+
+  @Value("${rabbitmq.routing.key}")
+  private String routingKey;
+  // Spring bean for rabbitmq queue
+  @Bean
+  public Queue queue() {
+    return new Queue(queue);
+  }
+
+  // Spring bean for rabbitmq exchange
+  @Bean
+  public TopicExchange exchange() {
+    return new TopicExchange(exchange);
+  }
+
+  // Spring bean for binding exchange with queue using routing key.
+  @Bean
+  public Binding bind() {
+    return BindingBuilder.bind(queue()).to(exchange()).with(routingKey);
+  }
+}
